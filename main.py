5419238badc4bb25823dde86253caf066a4d5479
Keyon Ashby <ashbykey000@warren.k12.in.us>
2024-11-08 09:18:20 -0500

    1st

---
+++ main.py
@@ -0,0 +1,19 @@
+#SECTION 1
+fruit_tuple = ("apple", "banana", "cherry", "date")
+print(fruit_tuple[0])
+print(fruit_tuple[3])
+
+
+
+#SECTION 2
+num_tuple = (3, 5, 3, 2, 8, 3, 7)
+print(num_tuple.count(3))
+print(num_tuple.index(8))
+#SECTION 3
+person_info = "Alice, 28, Engineer"
+name = "Alice"
+age = 28
+job = "Engineer"
+
+print(person_info)
+
